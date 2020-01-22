41ca6a2e7124296e786fd7dd5f77dc57dbdb549f
Livingstone Asabahebwa <ahebwa49@gmail.com>
2020-01-22 15:55:17 +0300

    add a worldcup json file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      geoData: null
+      data: {
+        geoData: null,
+        cupData: null
+      }
     };
   }
   componentDidMount() {
@@ -13,16 +16,29 @@ class App extends React.Component {
       "https://raw.githubusercontent.com/ahebwa49/geo_mapping/master/src/world_countries.json"
     )
       .then(response => response.json())
-      .then(data =>
-        this.setState({
-          geoData: data
-        })
-      )
+      .then(data => console.log(data))
       .catch(error => console.log(error));
+    //   Promise.all([
+    //     fetch(
+    //       "https://raw.githubusercontent.com/ahebwa49/geo_mapping/master/src/world_countries.json"
+    //     ),
+    //     fetch(
+    //       "https://raw.githubusercontent.com/ahebwa49/geo_mapping/master/src/world_cup_geo.tsv"
+    //     )
+    //   ])
+    //     .then(responses => Promise.all(responses.map(resp => resp.json())))
+    //     .then(([geoData, cupData]) => {
+    //       console.log(geoData);
+    //       this.setState({
+    //         data: {}
+    //       });
+    //     })
+    //     .catch(error => console.log(error));
+    // }
   }
   render() {
-    const { geoData } = this.state;
-    return <div>{geoData && <WorldMap data={geoData} />}</div>;
+    const { data } = this.state;
+    return <div>{data.geoData && <WorldMap data={data.geoData} />}</div>;
   }
 }
 export default App;

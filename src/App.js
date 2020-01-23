9d1f1f5c3292bcca84547747689312bf1effd2d4
Livingstone Asabahebwa <ahebwa49@gmail.com>
2020-01-23 10:27:19 +0300

    add size to the circles

--- src/App.js
+++ src/App.js
@@ -12,33 +12,30 @@ class App extends React.Component {
     };
   }
   componentDidMount() {
-    fetch(
-      "https://raw.githubusercontent.com/ahebwa49/geo_mapping/master/src/world_countries.json"
-    )
-      .then(response => response.json())
-      .then(data => console.log(data))
+    Promise.all([
+      fetch(
+        "https://raw.githubusercontent.com/ahebwa49/geo_mapping/master/src/world_countries.json"
+      ),
+      fetch(
+        "https://raw.githubusercontent.com/ahebwa49/geo_mapping/master/src/world_cup_geo.json"
+      )
+    ])
+      .then(responses => Promise.all(responses.map(resp => resp.json())))
+      .then(([geoData, cupData]) => {
+        // console.log(geoData);
+        console.log(cupData);
+        this.setState({
+          data: {
+            geoData: geoData,
+            cupData: cupData
+          }
+        });
+      })
       .catch(error => console.log(error));
-    //   Promise.all([
-    //     fetch(
-    //       "https://raw.githubusercontent.com/ahebwa49/geo_mapping/master/src/world_countries.json"
-    //     ),
-    //     fetch(
-    //       "https://raw.githubusercontent.com/ahebwa49/geo_mapping/master/src/world_cup_geo.tsv"
-    //     )
-    //   ])
-    //     .then(responses => Promise.all(responses.map(resp => resp.json())))
-    //     .then(([geoData, cupData]) => {
-    //       console.log(geoData);
-    //       this.setState({
-    //         data: {}
-    //       });
-    //     })
-    //     .catch(error => console.log(error));
-    // }
   }
   render() {
     const { data } = this.state;
-    return <div>{data.geoData && <WorldMap data={data.geoData} />}</div>;
+    return <div>{data.geoData && <WorldMap data={data} />}</div>;
   }
 }
 export default App;
--- src/WorldMap.js
+++ src/WorldMap.js
@@ -9,8 +9,6 @@ class WorldMap extends React.Component {
   componentDidMount() {
     const { data } = this.props;
 
-    console.log(data);
-
     const svg = d3
       .select(this.refs.chart)
       .append("svg")
@@ -28,13 +26,61 @@ class WorldMap extends React.Component {
 
     const map = svg
       .selectAll("path")
-      .data(data.features)
+      .data(data.geoData.features)
       .enter()
       .append("path")
       .attr("d", path)
       .style("fill", "rgb(9, 157, 217)")
       .style("stroke", "black")
       .style("stroke-width", 0.5);
+
+    const nested = d3
+      .nest()
+      .key(d => d.year)
+      .rollup(leaves => {
+        const total = d3.sum(leaves, d => d.attendance);
+
+        const coords = leaves.map(d => projection([+d.long, +d.lat]));
+
+        const center_x = d3.mean(coords, d => d[0]);
+
+        const center_y = d3.mean(coords, d => d[1]);
+
+        return {
+          attendance: total,
+          x: center_x,
+          y: center_y
+        };
+      })
+      .entries(data.cupData);
+
+    const attendance_extent = d3.extent(nested, d => d.value["attendance"]);
+
+    const rScale = d3
+      .scaleSqrt()
+      .domain(attendance_extent)
+      .range([0, 8]);
+
+    console.log(nested);
+
+    svg
+      .append("g")
+      .attr("class", "bubble")
+      .selectAll("circle")
+      .data(
+        nested.sort(function(a, b) {
+          return b.value["attendance"] - a.value["attendance"];
+        })
+      )
+      .enter()
+      .append("circle")
+      .attr("fill", "rgb(247, 148, 42)")
+      .attr("cx", d => d.value["x"])
+      .attr("cy", d => d.value["y"])
+      .attr("r", d => rScale(d.value["attendance"]))
+      .attr("stroke", "black")
+      .attr("stroke-width", 0.7)
+      .attr("opacity", 0.7);
   }
 
   render() {
